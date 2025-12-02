c4b87b7ba39cf5c05470ba997d353cfbe78aba2d
Edison Andres Perez Martinez <edisonandresperezmartinez@gmail.com>
2025-12-02 10:48:18 -0500

    seccion 11 - memorizacion y optimizaciones - useMemo

---
+++ 04-hooks-app/src/06-memos/MemoHook.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const MemoHook = () => {
+  return (
+    <div className='bg-gradient flex flex-col gap-4'>
+        <h1 className='text-2xl font-thin text-white'>
+            memoHook
+        </h1>
+
+
+    </div>
+  )
+}
--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -6,13 +6,14 @@ import { createRoot } from "react-dom/client";
 //import { PokemonPage } from "./03-exalmples/PokemonPage";
 
 import "./index.css";
+import { MemoHook } from "./06-memos/MemoHook";
 //import { FocusScreen } from "./04-useRef/FocusScreen";
 //import { TrafficLight } from "./01-useState/TrafficLight";
 //import { TrafficLightWithEffect } from "./02-useEffect/TrafficLightWithEffect";
 //import { TrafficLightWithEffectWithHook } from "./02-useEffect/TrafficLightWithEffectWithHook";
 
 //import { TasksApp } from "./05-useReducer/TaskApp";
-import { ScrambleWords } from "./05-useReducer/ScrambleWords";
+//import { ScrambleWords } from "./05-useReducer/ScrambleWords";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -26,7 +27,9 @@ createRoot(document.getElementById("root")!).render(
 
     {/*<TasksApp/>*/}
 
-  <ScrambleWords/>
+ {/* <ScrambleWords/>*/}
+
+ <MemoHook/>
 
 
   </StrictMode>
