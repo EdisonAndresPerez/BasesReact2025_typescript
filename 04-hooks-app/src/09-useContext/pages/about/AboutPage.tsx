de19876809908242f3a999fd33af5e32fb9ec6eb
Edison Andres Perez Martinez <edisonandresperezmartinez@gmail.com>
2025-12-04 14:20:55 -0500

    con estos cambios de react router no perdemos nuestro estado entre navegacion de paginas

--- 04-hooks-app/src/09-useContext/pages/about/AboutPage.tsx
+++ 04-hooks-app/src/09-useContext/pages/about/AboutPage.tsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router'
+import React from "react";
+import { Link } from "react-router";
 
 export const AboutPage = () => {
-  console.log('no se puede recargar la pagina ')
+  console.log("no se puede recargar la pagina ");
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen'>
-      <h1 className='text-4xl font-bold'>Pagina Sobre mi</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold">Pagina Sobre mi</h1>
       <hr />
 
-      <div className='flex flex-col gap-2'>
-        <Link to="profile">Perfil</Link>
-        <Link to="login">Iniciar sesion</Link>
+      <div className="flex flex-col gap-2">
+        <Link to="profile" className="hover:text-blue-500 underline text-2xl">
+          Perfil
+        </Link>
+        <Link to="login" className="hover:text-blue-500 underline text-2xl">
+          Iniciar sesion
+        </Link>
       </div>
-
     </div>
-  )
-}
+  );
+};
