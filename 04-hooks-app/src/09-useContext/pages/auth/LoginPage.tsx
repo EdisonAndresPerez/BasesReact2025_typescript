96c5e25c5a746646d21e271f5fadcd8faf03caa5
Edison Andres Perez Martinez <edisonandresperezmartinez@gmail.com>
2025-12-04 15:06:24 -0500

    compartir estado global entre componentes - contexto y proveedores

--- 04-hooks-app/src/09-useContext/pages/auth/LoginPage.tsx
+++ 04-hooks-app/src/09-useContext/pages/auth/LoginPage.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Link } from "react-router";
 
 export const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div className="flex flex-col items-center min-h-screen">
+      <h1 className="text-4xl font-bold">Iniciar sesion</h1>
+
+      <hr />
+
+      <form className="flex flex-col gap-2 my-10">
+        <Input type="number" placeholder="ID del usuario"></Input>
+
+        <Button type="submit">Ingresar</Button>
+      </form>
+      <Link to="/about">
+      <Button variant="ghost">Volver al inicio</Button>
+      </Link>
+    </div>
+  );
+};
--- 04-hooks-app/src/09-useContext/pages/profile/ProfilePage.tsx
+++ 04-hooks-app/src/09-useContext/pages/profile/ProfilePage.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router";
 
 export const ProfilePage = () => {
   return (
-    <div>ProfilePage</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl">Perfil del usuario</h1>
+      <hr />
+
+      <pre className="my-4 w-[80%] overflow-x-auto">
+        {JSON.stringify({}, null, 2)}
+      </pre>
+
+      <div className=" flex gap-2">
+        <Link to="/about">
+          <Button variant="destructive">Salir</Button>
+        </Link>
+        <Link to="/about">
+          <Button variant="ghost">Volver al Inicio</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
