5a3b634ff320a23dd698afcae84bb14ed67135d8
Edison Andres Perez Martinez <edisonandresperezmartinez@gmail.com>
2025-12-13 14:50:39 -0500

    usando Outlet de react router

--- 05-videoJuegos-app/src/videojuegos/layouts/VideoJuegosLayout.tsx
+++ 05-videoJuegos-app/src/videojuegos/layouts/VideoJuegosLayout.tsx
@@ -1,3 +1,7 @@
+import { Outlet } from "react-router";
+
 export const VideoJuegosLayout = () => {
-  return <div className=" bg-amber-800">hola mundo</div>;
+  return <div className=" bg-amber-800">hola mundo
+    <Outlet/>
+  </div>;   
 };
