654fcf4d25f39625b524ffa467494a37ed7ba497
edison andres perez martinez <edisonandresperezmartinez@gmail.com>
2026-04-13 07:50:16 -0500

    Haciendo ejercicios para mejorar pensamiento logico y las habilidades en la programacion

--- 01-Reforzamiento2025/src/bases/01-const-let.ts
+++ 01-Reforzamiento2025/src/bases/01-const-let.ts
@@ -1,4 +1,3 @@
-
 //-----------------------------------
 
 const primerNombre = "Edison";
@@ -8,21 +7,85 @@ const nombreCompleto = `${primerNombre} ${segundoNombre}`;
 console.log(nombreCompleto);
 
 const edad = 21;
-console.log(edad)
-
+console.log(edad);
 
 //-----------------------------------
 // 🧩 Tipos explícitos en TypeScript
+
 // Sirven para definir de forma manual el tipo de dato que una variable puede almacenar.
+
 // Se usa la sintaxis: nombreVariable: tipoDato = valor;
+// Ejemplo de tipo de dato: string, number, boolean, etc.
+
 // Esto permite que el compilador verifique errores de tipo antes de ejecutar el código.
-// Ejemplo: string, number, boolean, etc.
 // 🔍 Ideal para tener un código más seguro, legible y fácil de mantener.
+
+// Ejemplo de tipos explícitos en TypeScript
 const comidaFavorita: string = "Hamburguesa de tierra querida";
 const HamburguesaComidas: number = 3;
+
+// Ejemplo de error de tipo
+// const errorTipo: number = "Esto es un string"; // Esto generará un error de tipo porque se está asignando un string a una variable que espera un número.
+// const errorTipo = 'Edison perez';
 
 //-----------------------------------
 
 //Buscarletra a un string
-const buscarLetraE = segundoNombre.includes('e');
-console.log({buscarLetraE, comidaFavorita, HamburguesaComidas});
+const buscarLetraE = segundoNombre.includes("e");
+console.log({ buscarLetraE, comidaFavorita, HamburguesaComidas });
+
+//------------------------------------
+
+const nombre = "Edison";
+const apellido = "Perez";
+const miEdad: number = 22;
+const esDesarrollador: boolean = true;
+const fechaNacimiento = new Date("2003-11-20");
+const libroFavorito = "Los chicos malos tienen buenas historias";
+const peliculaFavorita: string = "Never Back Down";
+const ciudadFavorita = "Medellín";
+
+const informacionPersonal = `holaaa soy ${nombre} ${apellido}, tengo ${miEdad} años, soy desarrollador: ${esDesarrollador}, nací el ${fechaNacimiento.toDateString()}, mi libro favorito es "${libroFavorito}", mi película favorita es '${peliculaFavorita}' y mi ciudad favorita es ${ciudadFavorita}.`;
+
+console.log(informacionPersonal)
+console.log({miEdad, esDesarrollador, peliculaFavorita})
+
+//------------------------------------
+
+const nombreCompleto2 = 'Edison Andres Perez Martinez';
+const buscarLetraA = nombreCompleto.includes('a');
+const buscarPrimerNombre = nombre.includes('Edison');
+
+console.log({ buscarLetraA, buscarPrimerNombre });
+
+
+
+//-----------------------------------
+
+const producto = "Laptop";
+const precio = 2500000;
+const cantidad = 2;
+
+console.log(`persona numero 2 compraste: ${producto} a un precio de ${precio} por unidad. Total a pagar: ${precio * cantidad}`)
+
+
+// const a: number = "veinte"; => esta variable esta mal asginada por que la constante a espera un numero typado. pero la respuesta es un string.
+// const b: string = 100; => mal asignada por que la constante b espera un string pero se le asigna un numero.
+// const c: boolean = false; => esta variable esta bien asignada por que se le asigna un valor booleano (typado) y la respuesta es un booleano.
+// const d: string = `Hola mundo`; => esta variable esta bien asignada por que se le asigna un valor string (typado) y la respuesta es un string.
+
+
+//-----------------------------------
+
+//Perfil jugador
+const nombreJugador: string = 'DarkOzuFull';
+const nivelJugador: number = 200;
+const puntuacionJugador: number = 1500;
+const personaJugador: string = 'Invocador';
+const jugadorActivo: boolean = true;
+
+const perfilJugador = `El jugador ${nombreJugador} es un ${personaJugador} de nivel ${nivelJugador} con una puntuación de ${puntuacionJugador}. ¿Está activo? ${jugadorActivo}`;
+console.log(perfilJugador);
+
+const buscarLetraO = nombreJugador.includes('O');
+console.log({ buscarLetraO });
--- 01-Reforzamiento2025/src/bases/02-template-string.ts
+++ 01-Reforzamiento2025/src/bases/02-template-string.ts
@@ -1,5 +1,5 @@
 const miNombre = 'Andres';
-const miApellido = "P'erez"
+const miApellido = "Perez"
 
 const fullNombre = `El nombre es:
  ${miNombre} ${miApellido}
@@ -15,3 +15,54 @@ const ciudad = "ibague";
 const mensaje = "hola me llamo andres, \nmi videojuego favorito es " + videoJuegoFavorito + ". \njuego en la consola " + consola + " y vivo en la ciudad de " + ciudad;
 console.log(mensaje);
 
+
+//-----------------------------
+
+const animal = "perro";
+const sonido = "guau";
+const mensajeAnimal = `El ${animal} hace ${sonido}`;
+console.log(mensajeAnimal)
+
+
+//-----------------------------
+
+const videoJuegoFavorito2 = "Minecraft";
+const edadComienzo = 10;
+const ciudadComprado = "Ibague";
+
+const mensaje2 = ` videoJuego: ${videoJuegoFavorito2}
+ edad de compora: ${edadComienzo}
+ ciudad de compra: ${ciudadComprado}
+`
+console.log(mensaje2)
+
+
+//-----------------------------
+
+const precioBase = 80000;
+const iva = 0.19;
+const precioFinal = `El precio final del producto es: ${precioBase + (precioBase * iva)}`;
+console.log(precioFinal)
+
+//-----------------------------
+
+const esPremium = false;
+const usuario = "Andres";
+
+const mensajeUsuario = `El usuario ${usuario} es ${esPremium ? "premium" : "no premium"}`;
+//dentro del template con un ternario ? :
+console.log(mensajeUsuario)
+
+
+
+
+
+const producto = "Laptop";
+const cantidad = 2;
+const precioUnitario = 100000;
+const ciudadCompra = "Ibague";
+
+const mensajeCompra = `El cliente compro ${cantidad} unidades de ${producto}. a un precio unitario de ${precioUnitario}
+En la ciudad de ${ciudadCompra}. El precio total es: ${cantidad * precioUnitario} 
+El subtotal es: ${cantidad * precioUnitario}`
+console.log(mensajeCompra)
